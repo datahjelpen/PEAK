7875ea3e449daa82872c23c909cb51e0ed0bcbc5
Bear Hagen <post@bearhagen.com>
2017-02-02 12:57:21 +0100

    Norwegian locales

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module DatahjelpenOrg
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.default_locale = :no_nb
   end
 end
