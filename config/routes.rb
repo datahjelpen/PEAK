f1acd2d07f78c9af459b2e257ca0e79b62c3c4f6
Bear Hagen <post@bearhagen.com>
2017-02-02 12:53:23 +0100

    Added admin controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'admin', to: 'admin#index'
+
+  namespace :admin do
+    resources :posts
+    resources :post_categories
+    resources :post_types
+    resources :post_tags
+  end
 end
