9da170f96abf3f3ac0d8582956359d70ba889ec4
Bear Hagen <post@bearhagen.com>
2017-02-14 13:34:40 +0100

    after_filter is old, using after_action instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  after_filter :store_action
+  after_action :store_action
 
   # Store the action
   def store_action
