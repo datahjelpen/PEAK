200eca3c1ec38f1b6d427773369f64133e44ac1f
Bear Hagen <post@bearhagen.com>
2017-03-10 10:40:46 +0100

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  ## LOGIN & REGISTRATION ##
   devise_for :users, :controllers => {
     registrations: 'registrations',
     omniauth_callbacks: 'users/omniauth_callbacks'
