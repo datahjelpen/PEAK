9e18cd2f87e88cc46dc6b20761b385276f8748f1
Bjørnar Hagen <b@datahjelpen.no>
2017-07-01 19:24:53 +0200

    Added post table to post_tables migration

--- database/migrations/2017_07_01_164524_create_post_tables.php
+++ database/migrations/2017_07_01_164524_create_post_tables.php
@@ -13,6 +13,30 @@ class CreatePostTables extends Migration
      */
     public function up()
     {
+        Schema::create('posts', function (Blueprint $t) {
+            $t->increments('id');
+            $t->string('title');
+            $t->string('slug')->nullable(false)->change();
+            $t->text('text');
+            $t->text('excrept');
+            $t->text('extra_css');
+            $t->text('extra_js');
+            $t->integer('author')->unsigned();
+            $t->integer('template')->unsigned();
+            $t->integer('microdata')->unsigned();
+            $t->integer('custom_meta')->unsigned();
+            $t->integer('rights')->unsigned();
+            $t->boolean('comments');
+            $t->integer('status')->unsigned();
+
+            $t->integer('post_type')->unsigned();
+            $t->foreign('post_type')->references('id')->on('post_types')->onDelete('cascade');
+
+            $t->unique( ['slug','post_type'] );
+
+            $t->timestamps();
+        });
+
         Schema::create('post_types', function (Blueprint $t) {
             $t->increments('id');
             $t->string('name');
