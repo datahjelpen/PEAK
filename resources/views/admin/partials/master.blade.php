586ba418941854522525e782d62fbc95663f6119
Bjørnar Hagen <b@datahjelpen.no>
2017-07-03 15:12:50 +0200

    Moved admin scripts into admin partial

--- resources/views/admin/partials/master.blade.php
+++ resources/views/admin/partials/master.blade.php
@@ -9,5 +9,7 @@
 	@include('partials.sidebar')
 
 	@include('partials.footer')
+	<script src="/js/jquery-3.2.1.min.js"></script>
+	<script src="/js/admin-global.js"></script>
 </body>
 </html>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,4 @@
 <footer>
-	<script src="/js/jquery-3.2.1.min.js"></script>
-	<script src="/js/admin-global.js"></script>
 	<div>
 		@yield('content-footer')
 	</div>
