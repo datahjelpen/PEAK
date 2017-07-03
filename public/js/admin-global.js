0712d1317c9ef16cb09d5e721db2c4cfadf8c4fe
Bjørnar Hagen <b@datahjelpen.no>
2017-07-03 17:39:46 +0200

    Opening a modal will now trigger autofocus

--- public/js/admin-global.js
+++ public/js/admin-global.js
@@ -3,6 +3,7 @@
 		$(document).on('click', '.modal-trigger', function() {
 			var modal = $(this.getAttribute('data-modal'));
 			modal.toggle();
+			modal.find('.autofocus').trigger('focus');
 
 			modal.on('click', '.modal-close', function() {
 				modal.off('click');
