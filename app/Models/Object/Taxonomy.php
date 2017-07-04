298f99498d7282242817bfa5a937144697e67810
Bjørnar Hagen <b@datahjelpen.no>
2017-07-04 03:44:58 +0200

    Added getSingle method to object taxonomy as Laravel was getting wrong
    Laravel would get a taxonomy belonging to another object type

--- app/Models/Object/Taxonomy.php
+++ app/Models/Object/Taxonomy.php
@@ -12,4 +12,8 @@ class Taxonomy extends Model
         'hierarchical',
         'object_type'
     ];
+
+	static function getSingle(Type $type, Taxonomy $taxonomy) {
+		return Taxonomy::where(['slug' => $taxonomy->slug, 'object_type' => $type->id])->first();
+	}
 }
