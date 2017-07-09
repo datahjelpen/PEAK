1d0aa35d86e1739f5d62e96af3b4344c5d6856c8
Bjørnar Hagen <b@datahjelpen.no>
2017-07-09 19:34:11 +0200

    types variable is now available in all admin pages

--- app/Http/Controllers/Admin/Controller.php
+++ app/Http/Controllers/Admin/Controller.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers\Admin;
 
 use Session;
+use \View;
 use \Illuminate\Http\Request;
 use \Illuminate\Support\Facades\Validator;
+
+use \App\Model\Object\Type;
+use \App\Model\Object\Taxonomy;
+use \App\Model\Object\Term;
 
 class Controller extends \App\Http\Controllers\Controller
 {
@@ -16,5 +21,7 @@ class Controller extends \App\Http\Controllers\Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->types = Type::all();
+        View::share('types', Type::all());
     }
 }
