eed21bc903ef9328b546241a27b3f82f3b72b625
Bjørnar Hagen <b@datahjelpen.no>
2017-07-09 03:47:43 +0200

    Added dropdown for term parent

--- resources/views/admin/builder/object/term/fields.blade.php
+++ resources/views/admin/builder/object/term/fields.blade.php
@@ -19,7 +19,24 @@
 <input type="text" id="object-term-slug" name="slug" placeholder="slug" value="{{ $term->slug }}">
 
 <label for="object-term-parent">{{ __('general.parent') }}</label>
-<input type="text" id="object-term-parent" name="parent" placeholder="parent" value="{{ $term->parent }}">
+
+<select name="parent" id="object-term-parent">
+	@if ($term->parent != null)
+		<option></option>
+		@foreach ($terms as $_term)
+			@if ($term->slug != $_term->slug)
+				<option value="{{ $_term->id }}" {{ $_term->id == $term->parent ? 'selected' : null }}>{{ $_term->name }}</option>
+			@endif
+		@endforeach
+	@else
+		<option selected></option>
+		@foreach ($terms as $_term)
+			@if ($term->slug != $_term->slug)
+				<option value="{{ $_term->id }}">{{ $_term->name }}</option>
+			@endif
+		@endforeach
+	@endif
+</select>
 
 <label for="object-term-template">{{ __('general.template') }}</label>
 <input type="text" id="object-term-template" name="template" placeholder="template" value="{{ $term->template }}">
