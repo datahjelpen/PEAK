6968a992be0ad8fe460aaefdeb8935bfd0f51551
Bjørnar Hagen <b@datahjelpen.no>
2017-07-16 14:13:10 +0200

    Routes now have Type, Term, Object and Taxonomy available

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,10 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+use \App\Model\Object\Type;
+use \App\Model\Object\Term;
+use \App\Model\Object\Object;
+use \App\Model\Object\Taxonomy;
 */
 
 Auth::routes();
