00cfc7da68328087777de5ecc8b5f686dbcd6192
Bear Hagen <b@datahjelpen.no>
2017-11-16 03:05:25 +0100

    Now resource including files using mix function

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -6,5 +6,5 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>{{ config('app.name', 'PEAK') }}</title>
-	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
 </head>
