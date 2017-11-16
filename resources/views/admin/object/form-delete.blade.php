13239ada7f66d3eccebe2a4e1ca3bab8189f4dec
Bear Hagen <b@datahjelpen.no>
2017-11-16 03:06:30 +0100

    Added icons to modals

--- resources/views/admin/object/form-delete.blade.php
+++ resources/views/admin/object/form-delete.blade.php
@@ -3,6 +3,12 @@
 	{{ csrf_field() }}
 	
 	<h1>{{ __('forms.confirm.delete.ask', ['name' => $object->name]) }}</h1>
-	<button class="autofocus" type="submit" autofocus>{{ __('forms.confirm.delete.yes') }}</button>
-	<button type="button" class="modal-close">{{ __('forms.confirm.delete.no') }}</button>
+	<button class="autofocus" type="submit" autofocus>
+		<i data-feather="{{ __('forms.confirm.delete.yes-icon') }}"></i>
+		<span>{{ __('forms.confirm.delete.yes') }}</span>
+	</button>
+	<button type="button" class="modal-close">
+		<i data-feather="{{ __('forms.confirm.delete.no-icon') }}"></i>
+		<span>{{ __('forms.confirm.delete.no') }}</span>
+	</button>
 </form>
--- resources/views/admin/object/index.blade.php
+++ resources/views/admin/object/index.blade.php
@@ -17,16 +17,19 @@
 					<div id="show-object-type-{{ $object->id }}" class="modal">
 						<a href="{{ route('object.show', [$type->slug, $object->slug]) }}">{{ __('general.actions.open') }}</a>
 						<a href="{{ route('object.show', [$type->slug, $object->slug]) }}" target="_blank">{{ __('navigation.actions.open_new_tab') }}</a>
+						<button class="modal-close"><i data-feather="x-square"></i></button>
 						@include('object.content-main', ['object' => $object])
 					</div>
 
 					<button class="modal-trigger" data-modal="#edit-object-type-{{ $object->id }}">{{ __('general.actions.edit') }}</button>
 					<div id="edit-object-type-{{ $object->id }}" class="modal">
+						<button class="modal-close"><i data-feather="x-square"></i></button>
 						@include('admin.object.form-edit', [$type->slug, $object->slug])
 					</div>
 
 					<button class="modal-trigger" data-modal="#delete-object-type-{{ $object->id }}">{{ __('general.actions.delete') }}</button>
 					<div id="delete-object-type-{{ $object->id }}" class="modal">
+						<button class="modal-close"><i data-feather="x-square"></i></button>
 						@include('admin.object.form-delete', [$type->slug, $object->slug])
 					</div>
 				</li>
