d6bc1e3e7ef79ef9d4f2dc4b5d68b4b2b41e70b2
Bear Hagen <b@datahjelpen.no>
2017-11-18 12:52:23 +0100

    Removed unused parts of status views

--- resources/views/admin/superadmin/object/status/content-main.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>This is a status</h1>
-<ul>
-	<li>id:       {{ $status->id }}</li>
-	<li>name:     {{ $status->name }}</li>
-	<li>slug:     {{ $status->slug }}</li>
-</ul>
--- resources/views/admin/superadmin/object/status/index.blade.php
+++ resources/views/admin/superadmin/object/status/index.blade.php
@@ -12,13 +12,6 @@
 			@foreach ($statuses as $status)
 				<li id="object-types-list-item-{{ $status->id }}" class="object-types-list-item">
 					{{ $status->name }}
-
- 					<button class="modal-trigger" data-modal="#show-object-type-{{ $status->id }}">{{ __('navigation.actions.view_quick') }}</button>
-					<div id="show-object-type-{{ $status->id }}" class="modal">
-{{-- 						<a href="{{ route('object.status.show', [$type->slug, $status->slug]) }}">{{ __('general.actions.open') }}</a>
-						<a href="{{ route('object.status.show', [$type->slug, $status->slug]) }}" target="_blank">{{ __('navigation.actions.open_new_tab') }}</a> --}}
-						@include('admin.superadmin.object.status.content-main', ['status' => $status])
-					</div>
 					
 					<button class="modal-trigger" data-modal="#edit-object-type-{{ $status->id }}">{{ __('general.actions.edit') }}</button>
 					<div id="edit-object-type-{{ $status->id }}" class="modal">
@@ -29,8 +22,6 @@
 					<div id="delete-object-type-{{ $status->id }}" class="modal">
 						@include('admin.superadmin.object.status.form-delete', [$type->slug, $status->slug])
 					</div>
-
-					{{-- <a href="{{ route('admin.object.terms', [$type->slug, $status->slug]) }}">Terms</a> --}}
 				</li>
 			@endforeach
 		</ul>
