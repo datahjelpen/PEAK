e101a8070bfeccf39515ab88aab0c621d29f48bd
Bear Hagen <b@datahjelpen.no>
2017-11-22 11:41:32 +0100

    Code spacing and indentation fix

--- resources/views/admin/partials/master.blade.php
+++ resources/views/admin/partials/master.blade.php
@@ -1,11 +1,11 @@
 @include('partials.head')
 <body>
-    @include('partials.flash-messages')
+	@include('partials.flash-messages')
 
 	@include('admin.partials.navigation')
-	
+
 	@include('partials.main')
-	
+
 	@include('partials.sidebar')
 
 	@include('partials.footer')
