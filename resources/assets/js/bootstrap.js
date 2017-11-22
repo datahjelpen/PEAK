728ade3d65ef2b9dd97970b85a0b47c877b21ead
Bear Hagen <b@datahjelpen.no>
2017-11-22 11:40:27 +0100

    Added turbolinks

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -18,7 +18,6 @@ try {
  */
 
 window.axios = require('axios');
-
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /**
@@ -35,20 +34,12 @@ if (token) {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
 
-/**
- * Echo exposes an expressive API for subscribing to channels and listening
- * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
- */
-
-// import Echo from 'laravel-echo'
-
-// window.Pusher = require('pusher-js');
-
-// window.Echo = new Echo({
-//     broadcaster: 'pusher',
-//     key: 'your-pusher-key'
-// });
+// Use turbolinks
+// Makes all a href links use ajax
+try {
+	window.turbolinks = require("turbolinks")
+	window.turbolinks.start()
+} catch (e) {}
 
 // Use feather icons
 try {
