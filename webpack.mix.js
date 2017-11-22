935377e2b3ca668145fd922fb6c5b4829c50ab2f
Bear Hagen <b@datahjelpen.no>
2017-11-22 11:40:54 +0100

    Minify resources during build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,6 @@
 let mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css')
-   .version();
+mix.js('resources/assets/js/app.js', 'public/js').minify('public/js/app.js').version();
+mix.sass('resources/assets/sass/app.scss', 'public/css', {
+	outputStyle: 'compressed'
+}).version();
