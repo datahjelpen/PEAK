e4c0c78b34c3626dd7a742d4803a13317703169d
Bear Hagen <b@datahjelpen.no>
2017-11-23 04:13:59 +0100

    Seperate admin and superadmin nav

--- resources/views/admin/partials/navigation.blade.php
+++ resources/views/admin/partials/navigation.blade.php
@@ -1,14 +1,15 @@
 @include('partials.navigation')
-<nav>
+<nav id="nav-superadmin">
 	<ul>
+		<li><strong>Superadmin</strong></li>
 		<li>
-			<span>Superadmin</span>
-			<ul>
-				<li>
-					<a href="{{ route('superadmin.item_types') }}">Types</a>
-				</li>
-			</ul>
+			<a href="{{ route('superadmin.item_types') }}">Types</a>
 		</li>
+	</ul>
+</nav>
+<nav id="nav-admin">
+	<ul>
+		<li><strong>Edit</strong></li>
 		@foreach ($item_types as $item_type)
 			<li>
 				<a href="{{ route('admin.items', ['item_type' => $item_type]) }}">{{ str_plural($item_type->name) }}</a>
