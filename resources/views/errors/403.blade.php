9a88dc6ad3ddc536d44e82f413ea49537f9e4c0a
Bear Hagen <b@datahjelpen.no>
2017-12-03 01:23:49 +0100

    Added error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,6 @@
+@extends('partials.master')
+
+@section('content-main')
+    <h1>Error 403, Unauthorized</h1>
+    <p>You do not have permission to view this page</p>
+@endsection
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,6 @@
+@extends('partials.master')
+
+@section('content-main')
+    <h1>Error 404, not found</h1>
+    <p>We could not find the page you are looking for</p>
+@endsection
