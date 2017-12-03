eca581a9343d66d168157aa19e547feca03f2257
Bear Hagen <b@datahjelpen.no>
2017-12-03 14:05:23 +0100

    Added JS guid generator

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -13,6 +13,15 @@ try {
 	console.log(e);
 }
 
+// Generate GUID with RFC4122 v4 complianc
+function generate_guid() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
+
+
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
