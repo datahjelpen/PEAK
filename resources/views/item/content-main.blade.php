9aefea07e8201f4e734b50a87eb5cf92d6d8d2d7
Bear Hagen <b@datahjelpen.no>
2017-12-04 11:06:09 +0100

    Updated single item page

--- resources/views/item/content-main.blade.php
+++ resources/views/item/content-main.blade.php
@@ -1,22 +1,3 @@
-<h1>This is an item</h1>
-{{ dump($item) }}
-
-<pre>
-	@foreach ($item->terms as $term)
-		{{ $term->name }}
-	@endforeach
-</pre>
-
-
-<ul>
-	<li>id:        {{ $item->id }}</li>
-	<li>name:      {{ $item->name }}</li>
-	<li>slug:      {{ $item->slug }}</li>
-	<li>template:  {{ $item->template }}</li>
-	<li>text:      {{ $item->text }}</li>
-	<li>excerpt:   {{ $item->excerpt }}</li>
-	<li>item_type: {{ $item->item_type }}</li>
-	<li>author:    {{ $item->author }}</li>
-	<li>comments:  {{ $item->comments }}</li>
-	<li>status:    {{ $item->status }}</li>
-</ul>
+<section id="user-content">
+	{!! $item->text !!}
+</section>
