9b19b1a8b313b2cf44ffb437805f9b69d49afd09
Bear Hagen <b@datahjelpen.no>
2017-12-11 01:25:24 +0100

    Users can now drag and drop files to upload. Added multi file support.
    Moved quill spesific code into its own file.

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,17 +10,8 @@
 try {
 	window.$ = window.jQuery = require('jquery');
 } catch (e) {
-	console.log(e);
+	console.error(e);
 }
-
-// Generate GUID with RFC4122 v4 complianc
-function generate_guid() {
-	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-		return v.toString(16);
-	});
-}
-
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -63,191 +54,19 @@ if (token) {
 // Use quill text editor
 
 
-import Quill from 'quill';
-import BlotFormatter, { AlignAction, DeleteAction, ResizeAction, ImageSpec } from 'quill-blot-formatter'
-Quill.register('modules/blotFormatter', BlotFormatter);
-
-class CustomImageSpec extends ImageSpec {
-	getActions() {
-		return [AlignAction, DeleteAction, ResizeAction];
-	}
-}
-
-try {
-	require('quill/themes/snow.js');
-
-	var fonts = ['serif', 'sans-serif', 'monospace'];
-	var toolbarOptions = [
-		[{ 'size': [] }, { 'font': fonts }],
-		[ 'bold', 'italic', 'underline'],
-		[{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'align': [] }],
-		[ 'link', 'image', 'video'],
-		[ 'formula', 'code-block' ],
-		[ 'clean' ]
-	];
-
-	var editors = document.querySelectorAll('.wysiwyg-editor');
-	var forms = [];
-	var editorsQuill = [];
-	var editorTargets = [];
-
-	for (var i = 0; i < editors.length; i++) {
-		var editor = editors[i];
-		var editorTarget = $(editor).next('.wysiwyg-editor-target');
-		var form = editorTarget[0].form;
-
-		if (!(forms.indexOf(form) > -1)) {
-			forms.push(form);
-		}
-
-		// Setup unique classname for all editor elements to ensure uniqueness and avoid duplicate toolbars
-		var random = 'unique_'+generate_guid();
-		var id = '#'+editor.getAttribute('id')+'.'+random;
-		editor.className += ' '+random;
-		editorTarget[0].className = ' '+random;
-
-
-		// Setup Quill editor
-		var quill_editor = new Quill(id, {
-			modules: {
-				toolbar: toolbarOptions,
-				blotFormatter: {
-					specs: [
-						CustomImageSpec,
-					],
-					toolbar: {
-						buttonStyle: {
-						},
-						svgStyle: {
-						}
-					}
-				}
-			},
-			theme: 'snow'
-		});
-		editorsQuill.push(quill_editor);
-		editorTargets.push(editorTarget);
-
-		// custom image upload button
-		quill_editor.getModule('toolbar').addHandler('image', function(e) {
-			selectLocalImage(this.quill);
-		});
-
-	}
-	
-	for (var i = 0; i < forms.length; i++) {
-		forms[i].addEventListener('submit', function(e) {
-			for (var i = 0; i < editorsQuill.length; i++) {
-				var contentHTML = editorsQuill[i].container.firstChild.innerHTML;
-				var contentDelta = editorsQuill[i].getContents();
-
-				if (contentDelta.length() > 1) {
-					editorTargets[i].val(contentHTML);
-				}
-			}
-
-			return false;
-		});
-
-	}
-
-	var loadingIndicatorURL = '/images/graphics/image-upload-loading.gif';
-
-	function insertLoadingIndicator(editor) {
-		var range = editor.getSelection();
-
-		if (range) {
-			range = range.index;
-		} else {
-			range = 1;
-		}
-
-		editor.insertEmbed(range, 'image', loadingIndicatorURL);
-	}
-
-
-	function removeLoadingIndicator(editor) {
-		var content = editor.getContents();
-
-		for (var i = 0; i < content.ops.length; i++) {
-			var image = content.ops[i].insert.image;
-
-			if (image && image == loadingIndicatorURL) {
-				delete content.ops[i];
-			}
-		}
-
-		editor.setContents(content);
-	}
-
-	function selectLocalImage(editor) {
-		var input = document.createElement('input');
-		input.setAttribute('type', 'file');
-		input.click();
-
-		// Listen upload local image and save to server
-		input.onchange = () => {
-			var file = input.files[0];
-			insertLoadingIndicator(editor);
-
-			// file type is only image.
-			if (/^image\//.test(file.type)) {
-				saveToServer(editor, file);
-			} else {
-				removeLoadingIndicator(editor);
-				console.warn('You could only upload images.');
-			}
-		};
-	}
-
-	function saveToServer(editor, file) {
-		var fd = new FormData();
-		fd.append('image', file);
-
-		$.ajax({
-			url:'/upload/image',
-			data: fd,
-			processData: false,
-			contentType: false,
-			type: 'POST',
-			headers: {
-				'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-			},
-			success: function(data){
-				insertToEditor(editor, data.url);
-			},
-			error: function(data){
-				removeLoadingIndicator(editor);
-			}
-		});
-	}
-
-	function insertToEditor(editor, url) {
-		var range = editor.getSelection();
-
-		if (range) {
-			range = range.index;
-		} else {
-			range = 1;
-		}
-
-		editor.insertEmbed(range, 'image', '/uploads/'+url);
-		removeLoadingIndicator(editor);
-	}
-} catch (e) {
-	console.log(e);
-}
-
 // Use feather icons
 try {
 	window.feather = require('feather-icons');
 	window.feather.replace();
-} catch (e) {}
+} catch (e) {
+	console.error(e);
+}
 
 // Modals.js
 try {
 	require('./modals.js');
 	require('./image-loading.js');
+	require('./wysiwyg.js');
 } catch (e) {
-	console.log(e);
+	console.error(e);
 }
---
+++ resources/assets/js/wysiwyg.js
@@ -0,0 +1,260 @@
+import Quill from 'quill';
+import BlotFormatter, { AlignAction, DeleteAction, ResizeAction, ImageSpec, IframeVideoSpec } from 'quill-blot-formatter'
+Quill.register('modules/blotFormatter', BlotFormatter);
+
+class CustomImageSpec extends ImageSpec {
+	getActions() {
+		return [AlignAction, DeleteAction, ResizeAction];
+	}
+}
+
+require('quill/themes/snow.js');
+
+var fonts = ['serif', 'sans-serif', 'monospace'];
+var toolbarOptions = [
+	[{ 'size': [] }, { 'font': fonts }],
+	[ 'bold', 'italic', 'underline'],
+	[{ 'list': 'ordered' }, { 'list': 'bullet'}, { 'align': [] }],
+	[ 'link', 'image', 'video'],
+	[ 'formula', 'code-block' ],
+	[ 'clean' ]
+];
+
+var editors = document.querySelectorAll('.wysiwyg-editor');
+var forms = [];
+var editorsQuill = [];
+var editorTargets = [];
+
+for (var i = 0; i < editors.length; i++) {
+	(function() {
+		var editor = editors[i];
+		var editorTarget = $(editor).next('.wysiwyg-editor-target');
+		var form = editorTarget[0].form;
+
+		if (!(forms.indexOf(form) > -1)) {
+			forms.push(form);
+		}
+
+		// Setup unique classname for all editor elements to ensure uniqueness and avoid duplicate toolbars
+		var random = 'unique_'+generate_guid();
+		var id = '#'+editor.getAttribute('id')+'.'+random;
+		editor.className += ' '+random;
+		editorTarget[0].className = ' '+random;
+
+		// Setup Quill editor
+		var quill_editor = new Quill(id, {
+			modules: {
+				toolbar: toolbarOptions,
+				blotFormatter: {
+					specs: [
+						CustomImageSpec,
+						IframeVideoSpec
+					],
+					toolbar: {
+						addButtonSelectStyle: true,
+						buttonStyle: {
+							display: 'inline-block',
+							width: '24px',
+							height: '24px',
+							background: 'green',
+							border: '1px solid #ff0000',
+							verticalAlign: 'middle',
+						},
+						svgStyle: {
+							display: 'inline-block',
+							width: '24px',
+							height: '24px',
+							background: 'green',
+							border: '1px solid #ff0000',
+							verticalAlign: 'middle',
+						},
+					}
+				}
+			},
+			theme: 'snow',
+		});
+		editorsQuill.push(quill_editor);
+		editorTargets.push(editorTarget);
+
+		// custom image upload button
+		quill_editor.getModule('toolbar').addHandler('image', function(e) {
+			selectLocalImage(this.quill);
+		});
+
+		// listen for drop and paste events
+		quill_editor.container.addEventListener('dragleave', function (e) { handleDragLeave(e, quill_editor) });
+		quill_editor.container.addEventListener('dragover', function (e) { handleDragOver(e, quill_editor) });
+		quill_editor.container.addEventListener('drop',      function (e) { handleDrop(e, quill_editor) });
+		quill_editor.container.addEventListener('paste',     function (e) { handlePaste(e, quill_editor) });
+	})();
+}
+
+for (var i = 0; i < forms.length; i++) {
+	forms[i].addEventListener('submit', function(e) {
+		for (var i = 0; i < editorsQuill.length; i++) {
+			var contentHTML = editorsQuill[i].container.firstChild.innerHTML;
+			var contentDelta = editorsQuill[i].getContents();
+
+			if (contentDelta.length() > 1) {
+				editorTargets[i].val(contentHTML);
+			}
+		}
+
+		return false;
+	});
+
+}
+
+
+function handleDragLeave(evt, editor) {
+	evt.preventDefault();
+	$(editor.container).removeClass('wysiwyg-editor-drag-active');
+}
+
+
+function handleDragOver(evt, editor) {
+	evt.preventDefault();
+	$(editor.container).addClass('wysiwyg-editor-drag-active');
+}
+
+
+function handleDrop(evt, editor) {
+	evt.preventDefault();
+	$(editor.container).removeClass('wysiwyg-editor-drag-active');
+
+	if (evt.dataTransfer && evt.dataTransfer.files && evt.dataTransfer.files.length) {
+		for (var i = 0; i < evt.dataTransfer.files.length; i++) {
+			insertLoadingIndicator(editor);
+			saveToServer(editor, evt.dataTransfer.files[i]);
+		}
+	}
+}
+
+function handlePaste(evt, editor) {
+	if (evt.clipboardData && evt.clipboardData.items && evt.clipboardData.items.length) {
+		console.log("paste", evt);
+		// this.readFiles(evt.clipboardData.items, dataUrl => {
+		// 	const selection = this.quill.getSelection();
+		// 	if (selection) {
+		// 		// we must be in a browser that supports pasting (like Firefox)
+		// 		// so it has already been placed into the editor
+		// 	}
+		// 	else {
+		// 		// otherwise we wait until after the paste when this.quill.getSelection()
+		// 		// will return a valid index
+		// 		setTimeout(() => this.insert(dataUrl), 0);
+		// 	}
+		// });
+	}
+}
+
+
+
+
+
+
+
+
+var loadingIndicatorURL = '/images/graphics/image-upload-loading.gif';
+
+function insertLoadingIndicator(editor) {
+	var range = editor.getSelection();
+
+	if (range) {
+		range = range.index;
+	} else {
+		range = 1;
+	}
+
+	editor.insertEmbed(range, 'image', loadingIndicatorURL);
+}
+
+
+function removeLoadingIndicator(editor) {
+	var content = editor.getContents();
+
+	for (var i = 0; i < content.ops.length; i++) {
+		var image = content.ops[i].insert.image;
+
+		if (image && image == loadingIndicatorURL) {
+			delete content.ops[i];
+		}
+	}
+
+	editor.setContents(content);
+}
+
+function selectLocalImage(editor) {
+	var input = document.createElement('input');
+	input.setAttribute('type', 'file');
+	input.setAttribute('multiple', true);
+	input.click();
+
+	// Listen upload local image and save to server
+	input.onchange = () => {
+		if (input.files && input.files.length) {
+			for (var i = 0; i < input.files.length; i++) {
+				var file = input.files[i];
+				insertLoadingIndicator(editor);
+
+				// file type is only image.
+				if (/^image\//.test(file.type)) {
+					saveToServer(editor, file);
+				} else {
+					removeLoadingIndicator(editor);
+					console.warn('You could only upload images.');
+				}
+			}
+		}
+
+	};
+}
+
+function saveToServer(editor, file) {
+	var fd = new FormData();
+	fd.append('image', file);
+
+	$.ajax({
+		url:'/upload/image',
+		data: fd,
+		processData: false,
+		contentType: false,
+		type: 'POST',
+		headers: {
+			'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+		},
+		success: function(data){
+			insertToEditor(editor, data.url);
+		},
+		error: function(data){
+			removeLoadingIndicator(editor);
+		}
+	});
+}
+
+function insertToEditor(editor, url) {
+	var range;
+
+	try {
+		range = editor.getSelection()
+	} catch (e) {
+		console.warn(e);
+	}
+
+	if (range) {
+		range = range.index;
+	} else {
+		range = 1;
+	}
+
+	editor.insertEmbed(range, 'image', '/uploads/'+url);
+	removeLoadingIndicator(editor);
+}
+
+// Generate GUID with RFC4122 v4 complianc
+function generate_guid() {
+	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+		var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+		return v.toString(16);
+	});
+}
