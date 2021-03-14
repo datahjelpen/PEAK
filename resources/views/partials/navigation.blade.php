b71b48eddb656cb52794f0c795d9c57bb2c1bfea
Bjørnar Hagen <bjornar@datahjelpen.no>
2021-03-14 13:37:39 +0100

    Add old uncommited changes

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -7,26 +7,26 @@
 			</a>
 		</li>
 		@if (Auth::check())
-			<li><a href="{{ route('admin.dashboard') }}">admin</a></li>
-			<li>
-				<a href="{{ route('profile') }}">
-					<div id="avatar" style="background-image: url('{{ $profile->image->url }}')"></div>
-					<span>{{ $profile->name_display }}</span>
-				</a>
-				<ul>
-					<li>
-						<a id="admin-logout-action" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
-							Logout
-						</a>
-						<form id="admin-logout-form" action="{{ route('logout') }}" method="POST">
-							{{ csrf_field() }}
-						</form>
-					</li>
-				</ul>
-			</li>
+		<li><a href="{{ route('admin.dashboard') }}">admin</a></li>
+		<li>
+			<a href="{{ route('profile') }}">
+				<div id="avatar" style="background-image: url('{{ $profile->image->url ?? null }}')"></div>
+				<span>{{ $profile->name_display ?? null }}</span>
+			</a>
+			<ul>
+				<li>
+					<a id="admin-logout-action" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('admin-logout-form').submit();">
+						Logout
+					</a>
+					<form id="admin-logout-form" action="{{ route('logout') }}" method="POST">
+						{{ csrf_field() }}
+					</form>
+				</li>
+			</ul>
+		</li>
 		@else
-			<li><a href="{{ route('login') }}">Login</a></li>
-			<li><a href="{{ route('register') }}">Register</a></li>
+		<li><a href="{{ route('login') }}">Login</a></li>
+		<li><a href="{{ route('register') }}">Register</a></li>
 		@endif
 	</ul>
 </nav>
